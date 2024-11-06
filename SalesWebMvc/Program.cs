75e336de967c35e08b30e32c4e424ad5f4ce3f53
aliawada <awadinha99@gmail.com>
2024-11-06 04:51:45 -0300

    Seeding Service

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
+using static System.Formats.Asn1.AsnWriter;
+
 var builder = WebApplication.CreateBuilder(args);
+
 // Obtenha a ConnectionString do appsettings.json
 var connectionString = builder.Configuration.GetConnectionString("SalesWebMvcContext");
 
 // Registre o DbContext com a ConnectionString
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 40))));
+
+// Register SeedingService and any other services you need
+builder.Services.AddScoped<SeedingService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -21,6 +27,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    var scope = app.Services.CreateScope();
+    SeedingService seedingService = scope.ServiceProvider.GetRequiredService<SeedingService>();
+    seedingService.Seed();
+}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
